40229b542314b565fa89e611d53ad6c5d64b3bf2
Isaac Trenk <isaact@inv360.com>
2023-01-19 10:10:08 -0500

    fixed index

--- bookmarklet/reader.js
+++ bookmarklet/reader.js
@@ -1,12 +1,11 @@
 (function () {
-
     const style = document.createElement('style');
     style.innerText = `
         body{
             width: 50%;
             text-align: center;
         }
-        
+
         *{
             font-family: 'Courier New', Courier, monospace;
         }
