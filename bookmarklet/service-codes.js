4b60624493cbbe611bb8b99bb63903b109554f20
Isaac Trenk <isaact@inv360.com>
2023-02-16 12:24:42 -0500

    service codes optinal/required

--- bookmarklet/service-codes.js
+++ bookmarklet/service-codes.js
@@ -9,6 +9,21 @@
         a.click();
     }
 
+    const myCookies = (function parseCookies() {
+        const cookies = document.cookie.split(';');
+        const result = {};
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim().split('=');
+            result[cookie[0]] = cookie[1];
+        }
+        return result;
+    }());
+
+
+    const myHeaders = new Headers();
+    myHeaders.append("x-csrf-token", myCookies['csrf-token']);
+    myHeaders.append("content-type", "application/json; charset=UTF-8");
+
 
     async function serviceNote(code) {
         const requestOptionsGet = {
@@ -25,42 +40,94 @@
         }
     }
 
-    function parseCookies() {
-        const cookies = document.cookie.split(';');
-        const result = {};
-        for (let i = 0; i < cookies.length; i++) {
-            const cookie = cookies[i].trim().split('=');
-            result[cookie[0]] = cookie[1];
+    async function fetchServiceCodeList() {
+        let page = 1;
+        const pages = [];
+        while(true){
+            const items = await (async function (){
+                "use strict";
+                const postData = {
+                    "excludeFromSearch": "<data><item>contactlabel</item></data>",
+                    "codeLabelIdCollection": [],
+                    "advancedSearchMessageBus": {
+                        "subscriptions": [
+                            {
+                                "source": "*",
+                                "messageName": "search-values-updated-out-of-hash-cycle",
+                                "active": true
+                            },
+                            {
+                                "source": "*",
+                                "messageName": "search-values-updated",
+                                "active": true
+                            },
+                            {
+                                "source": "*",
+                                "messageName": "search-value-displays-updated",
+                                "active": true
+                            }
+                        ],
+                        "trackedSubscriptions": []
+                    },
+                    "_advancedSearchPluginInitialized": true,
+                    "filterCount": 0,
+                    "effectiveDisplay": "",
+                    "dateTypeDisplay": "",
+                    "dirtyProperties": [],
+                    "isDirty": 0,
+                    "queryDirty": false,
+                    "_syncingWithHash": false,
+                    "_collectionArr": [
+                        "codeLabelId"
+                    ],
+                    "_staticSearchItems": [],
+                    "_syncingPageSize": false,
+                    "resetting": false,
+                    "codeLabelIds": "",
+                    "codeLabelIdIncluded": "",
+                    "codeLabelIdExcluded": "",
+                    "startDate": "2023-02-15",
+                    "endDate": "2023-02-15",
+                    "page": page,
+                    "pageSize": 10000,
+                    "_track": {
+                        "channel": "billingmanager",
+                        "action": "servicecodes/loadservicecodeslist",
+                        "source": "Desktop",
+                        "latitude": 0,
+                        "longitude": 0,
+                        "metric": 0
+                    },
+                    "_utcOffsetMinutes": 300
+                }
+                const requestOptionsPost = {
+                    method: 'POST',
+                    headers: myHeaders,
+                    body: JSON.stringify(postData),
+                    redirect: 'follow'
+                };
+                const res = await fetch("https://members.centralreach.com/api/?billingmanager.loadservicecodeslist", requestOptionsPost);
+                return await res.json();
+            }());
+            page++;
+            if(!items.items.length){
+                break;
+            }
+            pages.push(...items.items);
         }
-        return result;
+        return pages;
     }
-    const myCookies = parseCookies()
-
-    const myHeaders = new Headers();
-    myHeaders.append("x-csrf-token", myCookies['csrf-token']);
-    myHeaders.append("content-type", "application/json; charset=UTF-8");
-
-    const rawPost = "{\r\n    \"excludeFromSearch\": \"<data><item>contactlabel</item></data>\",\r\n    \"codeLabelIdCollection\": [],\r\n    \"advancedSearchMessageBus\": {\r\n        \"subscriptions\": [\r\n            {\r\n                \"source\": \"*\",\r\n                \"messageName\": \"search-values-updated-out-of-hash-cycle\",\r\n                \"active\": true\r\n            },\r\n            {\r\n                \"source\": \"*\",\r\n                \"messageName\": \"search-values-updated\",\r\n                \"active\": true\r\n            },\r\n            {\r\n                \"source\": \"*\",\r\n                \"messageName\": \"search-value-displays-updated\",\r\n                \"active\": true\r\n            }\r\n        ],\r\n        \"trackedSubscriptions\": []\r\n    },\r\n    \"_advancedSearchPluginInitialized\": true,\r\n    \"filterCount\": 0,\r\n    \"effectiveDisplay\": \"\",\r\n    \"dateTypeDisplay\": \"\",\r\n    \"dirtyProperties\": [],\r\n    \"isDirty\": 0,\r\n    \"queryDirty\": false,\r\n    \"_syncingWithHash\": false,\r\n    \"_collectionArr\": [\r\n        \"codeLabelId\"\r\n    ],\r\n    \"_staticSearchItems\": [],\r\n    \"_syncingPageSize\": false,\r\n    \"resetting\": false,\r\n    \"codeLabelIds\": \"\",\r\n    \"codeLabelIdIncluded\": \"\",\r\n    \"codeLabelIdExcluded\": \"\",\r\n    \"startDate\": \"2023-02-15\",\r\n    \"endDate\": \"2023-02-15\",\r\n    \"page\": 1,\r\n    \"pageSize\": 1000,\r\n    \"_track\": {\r\n        \"channel\": \"billingmanager\",\r\n        \"action\": \"servicecodes/loadservicecodeslist\",\r\n        \"source\": \"Desktop\",\r\n        \"latitude\": 0,\r\n        \"longitude\": 0,\r\n        \"metric\": 0\r\n    },\r\n    \"_utcOffsetMinutes\": 300\r\n}";
-
-    const requestOptionsPost = {
-        method: 'POST',
-        headers: myHeaders,
-        body: rawPost,
-        redirect: 'follow'
-    };
     try {
-        const res = await fetch("https://members.centralreach.com/api/?billingmanager.loadservicecodeslist", requestOptionsPost);
-        const resJson = await res.json();
-        const noteTemplates = await Promise.all(resJson.items.map(async code => {
+        const resJson = await fetchServiceCodeList();
+        const noteTemplates = await Promise.all(resJson.map(async code => {
             const template = await serviceNote(code.Id);
             const { noteTemplates } = template.results;
-            // console.log(template);
             return {
                 Id: code.Id,
                 Code: code.Code,
                 Description: code.Description,
-                'templates Required': noteTemplates.filter(note => note.dependency === 2).map(note => note.name).join('; '),
-                'templates Optional': noteTemplates.filter(note => note.dependency === 1).map(note => note.name).join('; ')
+                'Templates Required': noteTemplates.filter(note => note.dependency === 2).map(note => note.name).join('; '),
+                'Templates Optional': noteTemplates.filter(note => note.dependency === 1).map(note => note.name).join('; ')
             }
         }));
 
