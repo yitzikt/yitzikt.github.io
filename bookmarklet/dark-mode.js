9a4aa26efaed62b9a721cb32b33bef4e0c32f88e
Isaac Trenk <isaact@inv360.com>
2023-10-08 20:09:30 -0400

    made flash cards temp

--- bookmarklet/dark-mode.js
+++ bookmarklet/dark-mode.js
@@ -8,3 +8,49 @@
     `;
     document.head.append(style);
 }());
+
+
+
+
+// function filterObjectByNonEmptyStrings(obj) {
+//     return Object.entries(obj).reduce((acc, [key, value]) => {
+//         if (Array.isArray(value) ) {
+//             acc[key] = value;
+//         } else if (typeof value === 'string' && value.trim() !== '') {
+//             acc[key] = value;
+//         } else if (typeof value === 'object') {
+//             const filteredNestedObject = filterObjectByNonEmptyStrings(value);
+//             if (Object.keys(filteredNestedObject).length > 0) {
+//                 acc[key] = filteredNestedObject;
+//             }
+//         }
+//         return acc;
+//     }, {});
+// }
+
+
+
+// const myObject = {
+//     key1: 'value1',
+//     key2: '',
+//     key3: {
+//         subKey1: 'subValue1',
+//         subKey2: '',
+//         subKey3: {
+//             subSubKey1: 'subSubValue1',
+//             subSubKey2: ''
+//         },
+//         subKey4: '   ',
+//         subKey5: 'subValue5'
+//     },
+//     key4: '   ',
+//     keylist1: [1,2,3,],
+//     keylist2: [],
+//     key5: 'value5'
+// };
+
+// const filteredObject = filterObjectByNonEmptyStrings(myObject);
+// console.log(filteredObject);
+
+
+
