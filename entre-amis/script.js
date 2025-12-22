a3e91c4dfa58c5e78bac68a95173530935ce1a26
Isaac Trenk <isaact@attainaba.com>
2025-12-22 12:46:18 -0500

    Update broccoli kugel image in menu item to reflect accurate representation and enhance visual appeal.

--- entre-amis/script.js
+++ entre-amis/script.js
@@ -207,7 +207,7 @@ const menuItems = [
         name: "Bouchées de Brocoli Façon Kugel",
         description: "Delightful individual broccoli kugels baked to golden perfection in muffin form, offering a savory blend of tender broccoli, onions, and eggs with a crisp exterior and fluffy center – a charming, bite-sized twist on a beloved classic.",
         category: "accompagnements",
-        image: "images/coming-soon.jpg"
+        image: "images/chicken-cutlets.jpg"
     },
     // Drinks
     {
